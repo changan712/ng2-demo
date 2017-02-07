bb3c2f6851fc3c9ff6a437eb81a5d3ceb227acd5
viola <yuy@caicv.com>
2017-02-07 14:42:46 +0800

    禁止多次导入CoreModule

--- module-demo/src/app/core/core.module.ts
+++ module-demo/src/app/core/core.module.ts
@@ -1,8 +1,7 @@
-import {NgModule, ModuleWithProviders} from '@angular/core';
+import {NgModule, ModuleWithProviders, Optional, SkipSelf} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TitleComponent} from "./title/title.component";
 import {UserService, UserServiceConfig} from "./user.service";
-
 
 
 @NgModule({
@@ -14,6 +13,13 @@ import {UserService, UserServiceConfig} from "./user.service";
     providers: [UserService,]
 })
 export class CoreModule {
+
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+        if (parentModule) {
+            throw new Error(
+                'CoreModule is already loaded. Import it in the AppModule only');
+        }
+    }
 
     static forRoot(config: UserServiceConfig): ModuleWithProviders {
 
