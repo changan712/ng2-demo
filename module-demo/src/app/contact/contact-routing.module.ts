1776a983631c5114d83effe0d5392464498561e7
tinshee <tinshee@sohu.com>
2017-02-09 17:36:07 +0800

    测试 git config

--- module-demo/src/app/contact/contact-routing.module.ts
+++ module-demo/src/app/contact/contact-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule }        from '@angular/router';
 import { ContactComponent }    from './contact.component';
 
 @NgModule({
+
   imports: [RouterModule.forChild([
     { path: 'contact', component: ContactComponent }
   ])],
