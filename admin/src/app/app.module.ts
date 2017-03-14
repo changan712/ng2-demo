2ce2bb03f440666bb75569cc3548edc6e9f374c5
tinshee <tinshee@sohu.com>
2017-03-14 11:54:07 +0800

    cli bug fixed 2

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import './rxjs-extensions'
+
+import {AppComponent} from './app.component';
+import {RouterModule} from "@angular/router";
+import {routing} from "./app.routing";
+
+
+import {CoreModule} from "./core/core.module";
+import {LocationStrategy, HashLocationStrategy} from "@angular/common";
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        CoreModule,
+        routing
+    ],
+
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+}
