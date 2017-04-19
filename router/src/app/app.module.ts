69748172d6f7226bbdc686529f15e2acd0755e25
tinshee <tinshee@sohu.com>
2017-04-19 14:17:27 +0800

    router demo  2

--- router/src/app/app.module.ts
+++ router/src/app/app.module.ts
@@ -7,15 +7,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login.component';
 import { ComposeMessageComponent } from './compose-message.component';
 import { NotFoundComponent } from './not-found.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-import { AdminComponent } from './admin.component';
-import { ManageCrisesComponent } from './manage-crises/manage-crises.component';
-import { ManageHeresComponent } from './manage-heres/manage-heres.component';
-import { CrisisCenterHomeComponent } from './crisis-center-home/crisis-center-home.component';
-import { CrisisCenterComponent } from './crisis-center.component';
-import { CrisisDetailComponent } from './crisis-detail.component';
-import { CrisisListComponent } from './crisis-list.component';
+
 
 
 @NgModule({
@@ -24,15 +16,6 @@ import { CrisisListComponent } from './crisis-list.component';
     LoginComponent,
     ComposeMessageComponent,
     NotFoundComponent,
-    HeroDetailComponent,
-    AdminDashboardComponent,
-    AdminComponent,
-    ManageCrisesComponent,
-    ManageHeresComponent,
-    CrisisCenterHomeComponent,
-    CrisisCenterComponent,
-    CrisisDetailComponent,
-    CrisisListComponent,
 
   ],
   imports: [
